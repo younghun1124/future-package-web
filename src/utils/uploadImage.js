c8cca62a03e78b809dbb0a7b01a51854dcfaddce
DawnGlow <ferrater1013@gmail.com>
2025-01-01 16:38:07 +0900

    fix(env): 환경변수 json 파싱 방법 수정

--- src/utils/uploadImage.js
+++ src/utils/uploadImage.js
@@ -2,18 +2,27 @@ import { Storage } from '@google-cloud/storage';
 
 let storage;
 try {
-  const credentials = JSON.parse(process.env.GCP_SERVICE_ACCOUNT_KEY);
-  
-  // private_key 문자열 처리 수정
-  credentials.private_key = credentials.private_key.replace(/\\n/g, '\n');
-  
-  if (!credentials) {
-    throw new Error('GCP 서비스 계정 키가 설정되지 않았습니다.');
+  const credentials = {
+    type: "service_account",
+    project_id: process.env.GCP_PROJECT_ID,
+    private_key_id: process.env.GCP_PRIVATE_KEY_ID,
+    private_key: process.env.GCP_PRIVATE_KEY?.replace(/\\n/g, '\n'),
+    client_email: process.env.GCP_CLIENT_EMAIL,
+    client_id: process.env.GCP_CLIENT_ID,
+    auth_uri: "https://accounts.google.com/o/oauth2/auth",
+    token_uri: "https://oauth2.googleapis.com/token",
+    auth_provider_x509_cert_url: "https://www.googleapis.com/oauth2/v1/certs",
+    client_x509_cert_url: process.env.GCP_CLIENT_X509_CERT_URL,
+    universe_domain: "googleapis.com"
+  };
+
+  if (!credentials.private_key) {
+    throw new Error('GCP private key가 설정되지 않았습니다.');
   }
 
   storage = new Storage({
     projectId: process.env.GCP_PROJECT_ID,
-    credentials: credentials
+    credentials
   });
 } catch (error) {
   console.error('GCP Storage 초기화 오류:', error);
