fc66a663d1ec9587bb82cac4dc515f5b09f50379
YoungHun <younghun4218@gmail.com>
2025-01-16 22:40:09 +0900

    cdn 이미지 사용허용

--- next.config.js
+++ next.config.js
@@ -8,6 +8,9 @@ const nextConfig = {
     // TypeScript 검사 비활성화
     ignoreBuildErrors: true,
   },
+  images: {
+    domains: ['storage.googleapis.com'], // 외부 이미지 도메인 허용
+  }
 }
 
 module.exports = nextConfig 
