0bfced9339b83eb8c2f512ca9f7d8ac9012134c3
DawnGlow <ferrater1013@gmail.com>
2025-01-21 18:53:26 +0900

    revert(const): 상수 클래스 일단 유지

--- src/constants/futureItems.js
+++ src/constants/futureItems.js
@@ -1,14 +1,52 @@
-import { neon } from '@neondatabase/serverless';
-
-let cachedGifticonTypes = [];
-
-export const loadGifticonTypes = async () => {
-  if (cachedGifticonTypes.length === 0) {
-    const sql = neon(process.env.DATABASE_URL);
-    const gifticonTypes = await sql`SELECT * FROM future_gifticon_type`;
-    cachedGifticonTypes = gifticonTypes;
-  }
-  return cachedGifticonTypes;
+export const FUTURE_MOVIE_TYPES = {
+  ACTION: {
+    id: 1,
+    name: '범죄시골39',
+    description: '배추 밭에선 신발끈도 묶지 말라고!',
+    imageUrl: '/crimecountry39_poster.svg',
+    detailImageUrl: '/crimecountry39_ticket.svg',
+  },
+  ROMANCE: {
+    id: 2,
+    name: '아웃사이드 인 15',
+    description: '기쁨이와 라일리의 환갑잔치 대소동!'  ,
+    imageUrl: '/outsidein_poster.svg',
+    detailImageUrl: '/outsidein_ticket.svg',
+  },
 };
 
-export const FUTURE_GIFTICON_TYPES = () => cachedGifticonTypes; 
+export const FUTURE_GIFTICON_TYPES = {
+  IcecreamChicken: {
+    id: 1,
+    name: '삐체씨 팝핑 아이스크림 치킨',
+    description: '달콤한 아이스크림과 바삭한 치킨의 만남! 톡톡튀는 팝핑캔디까지!',
+    imageUrl: '/icecreamchicken_dish.svg',
+    detailImageUrl: '/icecreamchicken_coupon.svg'
+  },
+  DoorianCake: {
+    id: 2,
+    name: '숨쉰당 두리안 시루 케이크 ',
+    description: '이 냄새 뭐지...? 매력적이야.. 천상의 맛 두리안이 한가득!',
+    imageUrl: '/dooriancake_dish.svg',
+    detailImageUrl: '/dooriancake_coupon.svg',
+    
+  },
+  
+};
+
+export const FUTURE_INVENTION_TYPES = {
+  DestinyCompass: {
+    id: 1,
+    name: '운명의 상대 나침반',
+    description: '운명의 상대에게로 당신을 인도합니다',
+    imageUrl: '/destiny_compass.svg',
+    detailImageUrl: '/destiny_compass.svg'
+  },
+  BlueToothShower: {
+    id: 2,
+    name: '블루투스 샤워기',
+    description: '...누가 샤워기에 오렌지 주스 연결해놨어? 왜 샤워기로 피부에 비타민 공급 하는건데!',
+    imageUrl: '/bluetooth_shower.svg',
+    detailImageUrl: '/bluetooth_shower.svg',
+  },
+};
---
+++ src/constants/futureTypes.js
@@ -0,0 +1,14 @@
+export const FUTURE_MOVIE_TYPES = {
+  ACTION: 1,
+  ROMANCE: 2,
+};
+
+export const FUTURE_GIFTICON_TYPES = {
+  COFFEE: 1,
+  FOOD: 2,
+};
+
+export const FUTURE_INVENTION_TYPES = {
+  TELEPORT: 1,
+  FLYING_CAR: 2,
+}; 
