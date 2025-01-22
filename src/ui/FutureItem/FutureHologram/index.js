be141846e6f6eb484194ba18911429f9b6ab134d
YoungHun <younghun4218@gmail.com>
2025-01-23 01:45:53 +0900

    홀로그램 스위치 렌더링으로 변경

--- src/ui/FutureItem/FutureHologram/index.js
+++ src/ui/FutureItem/FutureHologram/index.js
@@ -1,10 +1,21 @@
 import FutureHologramEdit from "./FutureHologramEdit";
 import FutureHologramView from "./FutureHologramView";
 
-export default function FutureHologram({modalState, dataRef}) {
+export default function FutureHologram({modalState,isInbox, onDelete, setModalState, dataRef,handleInsertWithData}) {
     return (
-        modalState==='edit'?
-        <FutureHologramEdit dataRef={dataRef}/>:
-        <FutureHologramView dataRef={dataRef}/>
+        <>
+            {(() => {
+                switch (modalState) {
+                    case 'edit':
+                        return <FutureHologramEdit setModalState={setModalState} handleInsertWithData={handleInsertWithData} dataRef={dataRef} />;
+                    {/* case 'preview':
+                        return <FutureNotePreview handleInsertWithData={handleInsertWithData} onDelete={onDelete} setModalState={setModalState} isInbox={isInbox} dataRef={dataRef} />; */}
+                    case 'view':
+                        return <FutureHologramView handleInsertWithData={handleInsertWithData} onDelete={onDelete} setModalState={setModalState} isInbox={isInbox} dataRef={dataRef} />;
+                    default:
+                        return null;
+                }
+            })()}
+        </>
     );
 } 
