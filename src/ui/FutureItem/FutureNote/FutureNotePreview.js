acab1095cfa8ceecbb5b7d2fa62ebad5ad3975e2
YoungHun <younghun4218@gmail.com>
2025-01-24 23:51:16 +0900

    노트 버튼 수정

--- src/ui/FutureItem/FutureNote/FutureNotePreview.js
+++ src/ui/FutureItem/FutureNote/FutureNotePreview.js
@@ -44,7 +44,7 @@ export default function FutureNoteView({dataRef,data, isInbox,setModalState,onDe
     return (
         <div className="relative">
             <h2 className="text-3xl text-center  text-white mb-2">쪽지</h2>
-            <p className="text-white text-center mb-4">아 혹시 외계의 언어로 보내고싶다면 얘기해. 바꿔줄게</p>
+            {!isInbox && <p className="text-white text-center mb-4">아 혹시 외계의 언어로 보내고싶다면 얘기해. 바꿔줄게</p>}
             <div className="relative">
                 <Image 
                     src="https://storage.googleapis.com/future-box-cdn-public/futureitem/note/FutureNote_2x.webp" 
@@ -60,7 +60,13 @@ export default function FutureNoteView({dataRef,data, isInbox,setModalState,onDe
                     </p>
                 </div>
                 <div className="flex flex-col mt-2">
+                
+                {isInbox ? 
                 <div className="flex justify-between">
+                    <DoodleButton variant='white' onClick={()=>onDelete()}>뺄래요</DoodleButton>
+                    <DoodleButton className='self-center' onClick={()=>setModalState('edit')}>수정할래요</DoodleButton>
+                </div>
+                :<div className="flex justify-between">
                     <DoodleButton 
                         variant='white' 
                         className='self-center'
@@ -71,6 +77,7 @@ export default function FutureNoteView({dataRef,data, isInbox,setModalState,onDe
                     </DoodleButton>
                     <DoodleButton className='self-center' onClick={()=>handleInsertWithData()}>담을래요</DoodleButton>
                 </div>
+                }
                 </div>
             </div>
         </div>
