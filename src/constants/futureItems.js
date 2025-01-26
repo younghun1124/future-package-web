e518f4ecdaa99df931947f2f6c93e37083312a86
YoungHun <younghun4218@gmail.com>
2025-01-27 01:25:59 +0900

    기프티콘 이미지 cdn으로 변경

--- src/constants/futureItems.js
+++ src/constants/futureItems.js
@@ -20,30 +20,30 @@ export const FUTURE_GIFTICON_TYPES = {
     id: 1,
     name: '삐체씨 팝핑 아이스크림 치킨',
     description: '달콤한 아이스크림과 바삭한 치킨의 만남! 톡톡튀는 팝핑캔디까지!',
-    imageUrl: '/icecreamchicken_dish.svg',
-    detailImageUrl: '/icecreamchicken_coupon.svg'
+    imageUrl: 'https://storage.googleapis.com/future-box-cdn-public/futureitem/gifticon/icecream_chicken/icecreamchicken_2x.webp',
+    detailImageUrl: 'https://storage.googleapis.com/future-box-cdn-public/futureitem/gifticon/icecream_chicken/icreamchicken_coupon_2x.webp'
   },
   DoorianCake: {
     id: 2,
     name: '숨쉰당 두리안 시루 케이크 ',
     description: '이 냄새 뭐지...? 매력적이야.. 천상의 맛 두리안이 한가득!',
-    imageUrl: '/dooriancake_dish.svg',
-    detailImageUrl: '/dooriancake_coupon.svg',
+    imageUrl: 'https://storage.googleapis.com/future-box-cdn-public/futureitem/gifticon/doorian_cake/doorian_cake_2x.webp',
+    detailImageUrl: 'https://storage.googleapis.com/future-box-cdn-public/futureitem/gifticon/doorian_cake/dooriancake_coupon_2x.webp',
     
   },
   CRIMECOUNTRY39: {
     id: 3,
     name: '범죄시골39',
     description: '배추 밭에선 신발끈도 묶지 말라고!',
-    imageUrl: '/crimecountry39_poster.svg',
-    detailImageUrl: '/crimecountry39_ticket.svg',
+    imageUrl: 'https://storage.googleapis.com/future-box-cdn-public/futureitem/gifticon/crimecountry39/crimecountry39_poster_2x.webp',
+    detailImageUrl: 'https://storage.googleapis.com/future-box-cdn-public/futureitem/gifticon/crimecountry39/crimecountry39_ticket_2x.webp',
   },
   OUTSIDEIN: {
     id: 4,
     name: '아웃사이드 인 15',
     description: '기쁨이와 라일리의 환갑잔치 대소동!'  ,
-    imageUrl: '/outsidein_poster.svg',
-    detailImageUrl: '/outsidein_ticket.svg',
+    imageUrl: 'https://storage.googleapis.com/future-box-cdn-public/futureitem/gifticon/outsidein/outsidein_poster_2x.webp',
+    detailImageUrl: 'https://storage.googleapis.com/future-box-cdn-public/futureitem/gifticon/outsidein/outsidein_ticket_2x.webp',
   },
   
 };
